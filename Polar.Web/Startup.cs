9f2da100d5140e4971739333bb3089830e607a94
PolarPenguin <273544966@qq.com>
2015-11-13 09:08:33 +0800

    CreateProject

---
+++ Polar.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Polar.Web.Startup))]
+namespace Polar.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
